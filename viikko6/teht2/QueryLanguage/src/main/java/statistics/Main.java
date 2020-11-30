542dcedbd72327d1ae6d7392b3c049a12df4852c
jmlii <44909874+jmlii@users.noreply.github.com>
2020-11-30 22:06:04 +0200

    Viikko 6 tehtävä 3

--- viikko6/teht2/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/teht2/QueryLanguage/src/main/java/statistics/Main.java
@@ -16,8 +16,8 @@ public class Main {
             System.out.println(player);
         }
         
+        // tehtävä 2: All, Not, HasFewerThan
         System.out.println("Number of players : "+ stats.matches(new All()).size());
-
         
         Matcher m2 = new And( 
             new Not( new HasAtLeast(1, "goals") ), 
@@ -37,6 +37,26 @@ public class Main {
             System.out.println(player);
         }
 
+        // tehtävä 3: Or
+        Matcher m4 = new Or( new HasAtLeast(40, "goals"),
+            new HasAtLeast(60, "assists")
+        );  
+        System.out.println("Or:");
+        for (Player player : stats.matches(m4)) {
+            System.out.println(player);
+        }
         
+        Matcher m5 = new And(
+            new HasAtLeast(50, "points"),
+            new Or( 
+                new PlaysIn("NYR"),
+                new PlaysIn("NYI"),
+                new PlaysIn("BOS")
+            )
+        );
+        System.out.println("Or:");
+        for (Player player : stats.matches(m5)) {
+            System.out.println(player);
+        }
     }
 }
